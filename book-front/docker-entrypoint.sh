43893e249b471eee5c83c6fb182d17a2284547ee
romainsessa <romainsessa@gmail.com>
2026-07-06 22:26:06 +0200

    fix: support dynamic Heroku port

--- book-front/docker-entrypoint.sh
+++ book-front/docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+envsubst '$PORT' \
+< /etc/nginx/templates/default.conf.template \
+> /etc/nginx/conf.d/default.conf
+
+
 cat <<EOF > /usr/share/nginx/html/env.js
 window.env = {
   BOOK_BACK_URL: "${BOOK_BACK_URL}"
